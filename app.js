7752c5e82fc27fa2b6e1152867c401250dc4aa06
Mindss_0109 <mindula.20211227@iit.ac.lk>
2024-02-13 14:56:06 +0530

    setted up mongodb and tested the connection

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 const bookRouter = require('./src/routes/books/bookRoutes'); // Import bookRouter
+const { connectToDb, getDb } = require('./db'); // Import connectToDb and getDb functions from db.js
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Mounting bookRouter at /books/search path
 app.use('/books/search', bookRouter);
+
+// Connect to MongoDB database
+connectToDb((err) => {
+  if (err) {
+    console.error('Failed to connect to MongoDB:', err);
+    // Handle the error appropriately, e.g., by exiting the application
+    process.exit(1);
+  }
+  console.log('Connected to MongoDB successfully');
+});
+
+// Example route to test the database connection
+app.get('/', (req, res) => {
+  const db = getDb();
+  // Perform database operations using db instance
+  res.send('Hello World! Database connection is working.');
+});
 
 // Start the server
 const PORT = process.env.PORT || 3001;
--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 const { MongoClient } = require('mongodb');
 
-let dbConnection
-let uri = 'mongodb+srv://HarborReads:MongoDB@HR12@cluster0.nhckanx.mongodb.net/?retryWrites=true&w=majority'
+let dbConnection;
+let uri = 'mongodb+srv://HarborReads:MongoDB%40HR12@cluster0.nhckanx.mongodb.net/harborreads?retryWrites=true&w=majority';
 
 module.exports = {
     connectToDb: (cb) => {
         MongoClient.connect(uri)
           .then((client) => {
-            dbConnection = client.db()
-            return cb()
+            dbConnection = client.db();
+            // Pass the dbConnection to the callback
+            return cb(null, dbConnection);
           })
           .catch(err => {
-            console.log(err)
-            return cb(err)
-          })
-
+            console.log(err);
+            return cb(err);
+          });
     },
-    getDb : () => dbConnection
-}
+    getDb: () => dbConnection
+};
