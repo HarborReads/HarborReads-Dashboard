e14623d4e821e6e149df58c42af8d0eb853dd6d8
Mindss_0109 <mindula.20211227@iit.ac.lk>
2024-02-18 20:15:37 +0530

    implemented fnctionality for userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,3 +1,5 @@
 //These files define the HTTP endpoints and their corresponding controller actions using Express Router.
 //They organize routes based on functionality and separate concerns by delegating route logic to the appropriate controller functions.
 // For example, in a user routes file, you might define routes for user registration, login, profile updates, etc.
+const express = require('express');
+const router = express.Router();
