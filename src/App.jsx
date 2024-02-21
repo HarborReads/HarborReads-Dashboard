0c8cbd45a46dc1323267de2edd27eb49e9be6870
chamindu <cnewandith123@gmail.com>
2024-02-21 11:11:24 +0530

    Search page add

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,21 @@ import LeftBar from './component/LeftBar';
 import OpenScreen from './component/OpenScreen';
 import SignIn from './component/SignIn';
 import SignUp from './component/SignUp';
-
-
-
-
- import React from 'react'
+import SearchPage from './component/search/SearchPage';
 import RightBar from './component/RightBar/RightBar';
+
+
+import React from 'react'
+
  
  function App() {
    return (
     <div className="App bg-indigo-600 w-full h-screen overflow-hidden">
       
     <LeftBar/>
-    
     <RightBar/>
+    <SearchPage/>
+    
     
     </div>
    )
