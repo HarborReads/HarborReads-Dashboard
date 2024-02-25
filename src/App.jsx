e6a113cddccc2ae1a4756d77e0dd9cfd86579f7b
Mindss_0109 <mindula.20211227@iit.ac.lk>
2024-02-26 01:10:35 +0530

    Changed the Frame of the UI and Re-edited the UI of the search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import LeftBar from './component/LeftBar';
-import OpenScreen from './component/OpenScreen';
-import SignIn from './component/SignIn';
-import SignUp from './component/SignUp';
 import SearchPage from './component/search/SearchPage';
 import RightBar from './component/RightBar/RightBar';
 
+const App = () => {
+  return (
+    <Router>
+      <div className="grid grid-cols-2 md:grid-cols-5 gap-4">
+        <LeftBar className="LeftBar col-span-1 md:col-span-1" />
+        <div className="content-pane col-span-3 md:col-span-3">
+          <Routes>
+            <Route path="/" element={<SearchPage />} /> {/* Default route for ContentPane */}
+          </Routes>
+        </div>
+        <RightBar className="RightBar col-span-1 md:col-span-1" />
+      </div>
+    </Router>
+  );
+};
 
-import React from 'react'
-
- 
- function App() {
-   return (
-    <div className="App bg-indigo-600 w-full h-screen overflow-hidden">
-      
-    <LeftBar/>
-    <RightBar/>
-    <SearchPage/>
-    
-    
-    </div>
-   )
- }
- 
- export default App
- 
+export default App;
--- src/component/search/SearchPage.jsx
+++ src/component/search/SearchPage.jsx
@@ -1,43 +1,19 @@
 import React, { useState } from 'react';
-import data from './TemplateData.json';
+import SearchBar from './SearchBar';
+import SearchResults from './SearchResults';
+import SearchHistory from './SearchHistory';
 
 function SearchPage() {
   const [searchTerm, setSearchTerm] = useState('');
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="maincontainer w-[400px] md:w-[1000px] h-[725px] md:h-[725px] bg-black flex flex-col justify-between overflow-y-auto">
-        <div className='productContainer'>
-          <div className='flex justify-center items-center mt-5'>
-            <input
-              className='input w-[350px] md:w-[900px] h-[40px] md:h-[40px] rounded-full pl-4'
-              type='text'
-              placeholder='Search'
-              onChange={(event) => setSearchTerm(event.target.value)}
-            />
-          </div>
-          <div className="flex flex-wrap justify-center">
-            {data
-              .filter((val) => {
-                if (searchTerm === "") {
-                  return val;
-                } else if (
-                  val.title.toLowerCase().includes(searchTerm.toLowerCase())
-                ) {
-                  return val;
-                }
-              })
-              .map((val) => (
-                <div className="bg-white m-4 p-4 rounded-md" key={val.id}>
-                  <img src={val.image} alt="" className="h-48" />
-                  <h3>{val.title}</h3>
-                  <p className="price">${val.price}</p>
-                </div>
-              ))}
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <br/>
+      <SearchResults searchTerm={searchTerm} />
+      <br/>
+      <SearchHistory />
+    </> 
   );
 }
 
