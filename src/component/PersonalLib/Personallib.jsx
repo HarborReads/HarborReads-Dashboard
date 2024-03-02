95701d5bada057a2337b20a2a8363259407652f7
chamindu <cnewandith123@gmail.com>
2024-03-02 21:43:34 +0530

    change background colors and did some adjestments in personal library

--- src/component/PersonalLib/Personallib.jsx
+++ src/component/PersonalLib/Personallib.jsx
@@ -4,11 +4,11 @@ import data from '../search/TemplateData.json';
 function Personallib() {
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <div className="maincontainer w-[400px] md:w-[65%] h-[570px] md:h-[725px] bg-black flex flex-col justify-between overflow-y-auto">
-        <div className='productContainer'>
+      <div>
+        <div className='productContainer bg-gray-200'>
           <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-3 flex justify-center">
             <div className="relative flex">
-              <h1 className='w-full max-w-[408px] text-3xl md:text-5xl font-medium mt-5 text-white text-center'>Personal Library</h1>
+              <h1 className='w-full max-w-[408px] text-3xl md:text-5xl font-medium mt-5 text-black text-center'>Personal Library</h1>
             </div>
           </div>
           <div className="flex flex-wrap justify-center mt-7">
@@ -16,7 +16,6 @@ function Personallib() {
               <div key={index} className="bg-white m-4 p-4 rounded-md">
                 <img src={val.image} alt="" className="h-24 md:h-48" />
                 <h3 className='text-[10px] md:text-[20px] '>{val.title}</h3>
-                <p className="price">${val.price}</p>
               </div>
             ))}
           </div>
--- src/component/PersonalLib/PersonallibLeftBar.jsx
+++ src/component/PersonalLib/PersonallibLeftBar.jsx
@@ -13,39 +13,36 @@ function PersonallibLeftBar() {
     <li key={index}>
       <a
         href="#"
-        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+        className="flex items-center p-2 px-4 hover:bg-gradient-to-r from-nav-bg to-nav-brown- hover rounded-full"
       >
-        <span className="text-white mb-2">{button}</span>
+        <span className="text-black text-lg mb-2">{button}</span>
       </a>
     </li>
   ));
 
   return (
-    <div className="h-full w-64 px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+    <div className="h-full  px-3 py-4 overflow-y-auto bg-white  ">
       <div className='flex justify-center items-center mt-3 mb-5'>
-            <h1 className='text-white text-[30px] underline '>My Books</h1>
-          </div>
-      <aside >
+        <h1 className='text-black text-2xl underline'>My Books</h1>
+      </div>
+      <aside>
         <ul>
           {buttonList}
           <li>
             <a
               href="#"
-              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-black hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
-             
             </a>
           </li>
         </ul>
         <div>
-        <h1 className='text-white text-[20px] underline'>Name</h1>
-        <h1 className='text-white text-[15px] mt-3'>Add a Shelf :</h1>
-        <input className='text-[15px] rounded-2xl mr-2'></input>
-        <button className='text-white text-[15px] bg-blue-500 rounded-xl w-[40px]'>Add</button>
-      </div>
-
+          <h1 className='text-black text-xl underline'>Name</h1>
+          <h1 className='text-black text-base mt-3'>Add a Shelf :</h1>
+          <input className='text-black bg-gray-200 rounded-2xl mr-2'></input>
+          <button className='text-black text-base bg-blue-500 rounded-xl w-16'>Add</button>
+        </div>
       </aside>
-
     </div>
   );
 }
