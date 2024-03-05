d82ed92b4b30e9b1a00af7aac9458ca2df6a6a36
chamindu <cnewandith123@gmail.com>
2024-03-05 15:52:50 +0530

    fix error in chatbotdirect import chatbot

--- src/component/DashBoard/ChatbotDirect.jsx
+++ src/component/DashBoard/ChatbotDirect.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import chatbotImage from './../../assets/chatbotImg.png';
-import ChatBot from '../ChatBot';
+import ChatBot from '../ChatBot/ChatBot';
 
 const Chatbot = () => {
   return (
