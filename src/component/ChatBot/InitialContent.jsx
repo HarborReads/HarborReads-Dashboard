81f937828ade12fff5775b830f775dec0bea683d
Mindss_0109 <mindula.20211227@iit.ac.lk>
2024-03-06 12:40:53 +0530

    added navigation to route paths when the button is clicked on the chatbots initial content

--- src/component/ChatBot/InitialContent.jsx
+++ src/component/ChatBot/InitialContent.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import SendIcon from '@mui/icons-material/PresentToAll';
 import './ChatBot.css';
 
-function InitialContent(userName) {
+function InitialContent({userName,setChatType}) {
   const handleButtonClick = (value) => {
-    props.setChatType(value);
+    setChatType(value);
   };
   return (
     <div className="flex flex-col justify-center items-center h-full  animate-fadeI">
@@ -15,17 +15,17 @@ function InitialContent(userName) {
         <p className="text-2xl md:text-4xl font-bold text-slate-500">How can I help you today?</p>
       </div>
       <div className="chatgrid text-slate-500" >
-        <button className="button md:h-[9.375rem] h-[6rem]" style={{ fontSize: '1.25rem'}}>
+        <button className="button md:h-[9.375rem] h-[6rem]" style={{ fontSize: '1.25rem'}} onClick={()=>{handleButtonClick("newReadersChat")}}>
           New to reading?<br/> Need help finding a book?
           <SendIcon className="arrow" />
           <span className="click-to-send">Click to send</span>
         </button>
-        <button className="button md:h-[9.375rem] h-[5.625rem]" style={{ fontSize: '1.25rem'}}>
+        <button className="button md:h-[9.375rem] h-[5.625rem]" style={{ fontSize: '1.25rem'}} onClick={()=>{handleButtonClick("avidReadersChat")}}>
           Love reading?<br/> Let's find your next book!
           <SendIcon className="arrow" />
           <span className="click-to-send" >Click to send</span>
         </button>
-        <button className="button md:h-[9.375rem] h-[5.625rem] " style={{ fontSize: '1.25rem '}}>
+        <button className="button md:h-[9.375rem] h-[5.625rem] " style={{ fontSize: '1.25rem '}} onClick={()=>{handleButtonClick("bookChat")}}>
           Interested in chatting about books?
           <SendIcon className="arrow" />
           <span className="click-to-send">Click to send</span>
