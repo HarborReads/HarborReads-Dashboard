5d5a2717c707ba8017427d7fbfd93e25e387111f
VenuriJayasinghe <venuri.20211228@iit.ac.lk>
2024-03-16 16:21:31 +0530

    Jest Unit Testing

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -1,10 +1,10 @@
+
 const express = require('express');
 const router = express.Router();
 
 const {aRstartConversation,aRgenerateResponse,aRgenerateRecommendation} = require('../controllers/chatController/avidReadersChatController');
 const {nRstartConversation,nRgenerateResponse,nRgenerateRecommendation} = require('../controllers/chatController/newReadersChatController');
 const {bookChatStartConversation,bookChatGenerateResponse,bCgenerateRecommendation} = require('../controllers/chatController/bookChatController');
-
 
 router.post('/avidReadersChat/startConversation', aRstartConversation);
 router.post('/avidReadersChat/generateResponse', aRgenerateResponse);
@@ -16,8 +16,8 @@ router.post('/newReadersChat/generateResponse', nRgenerateResponse);
 router.post('/avidReadersChat/generateRecommendation',nRgenerateRecommendation);
 
 
-
-
+router.post('/bookChat/startConversation', bookChatStartConversation);
+router.post('/bookChat/generateResponse', bookChatGenerateResponse);
 
 
 module.exports = router;
